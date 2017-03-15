31bfdb0e1f13590ca54458648a4e3e131d01f94e
Josmel <josmel@onlinestudioproductions.com>
2017-03-15 16:21:14 -0500

    feat:all

--- ClearPar.php
+++ ClearPar.php
@@ -14,10 +14,6 @@ class ClearPar
     public function build($chain = null)
     {
 
-        $string = "ABC (Test1(even deeper) yes (this (works) too)) outside (((ins)id)e)";
-
-
-
         $string = "()())()";
         $paren_num = 0;
         $chars = str_split($string);
@@ -31,29 +27,10 @@ class ClearPar
                 $new_string .= $char;
             }
 
-         //   var_dump($new_string);exit;
-//            else if ($char == ')') $paren_num--;
-//            else if ($paren_num == 0) $new_string .= $char;
+
         }
-        echo trim($new_string);exit;
+        return trim($new_string);
 
-
-
-
-
-
-
-        try {
-            $newChain = null;
-            foreach (str_split($chain) as $v=>$k) {
-                var_dump($v,$k);exit;
-
-
-            }
-            return $newChain;
-        } catch (\Exception $ex) {
-            var_dump($ex->getMessage());
-        }
     }
 }
 
